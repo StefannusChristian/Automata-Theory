0f23c827480bc24fb19a61a74fee2643cf3e5bda
Stefannus Christian <schristian38@students.calvin.ac.id>
2022-12-08 16:34:42 +0700

    finishing and add error

--- finalProject/vm.py
+++ finalProject/vm.py
@@ -1,3 +1,4 @@
+from prettytable import PrettyTable,ALL
 class VirtualMachine:
     # Constructor
     def __init__(self):
@@ -7,8 +8,12 @@ class VirtualMachine:
     def get_vm_memory(self): return self.memory
 
     # setter untuk memory
-    def set_vm_memory(self, idx, value): self.memory[idx] = value
-    
+    def set_vm_memory(self, idx, value):
+        try: self.memory[idx] = value
+        except: 
+            print("\nERROR!")
+            print("Possible Error: Invalid Polish Notation!\n")
+        
     # method untuk membuang kurung dari string input
     def remove_braces(self, string): return string.replace("(","").replace(")","")
 
@@ -27,14 +32,18 @@ class VirtualMachine:
         """ 
         input_list = ['=', '0', '*', '3', '+', '1', '2']
         """
+        
+        # Remove empty spaces from string list
+        input_list = list(filter(None, input_list))
 
-        # membuat list yang isinya string dari 0 - 9
-        numbers = [str(i) for i in range(10)]
-
-        # iterasi input list nya, kalo dia string 0 - 9 maka ubah jadi integer
+        """ 
+        iterasi input list nya, kalo string nya bisa langsung di convert ke integer berarti dia angka
+        kalo gabisa berarti dia bukan 0 - 9 jadi kita pass aja
+        """
         for i in range(len(input_list)): 
-            if input_list[i] in numbers: input_list[i] = int(input_list[i])
-
+            try: input_list[i] = int(input_list[i])
+            except: pass
+        
         # input_list (yang di return) = ['=', 0, '*', 3, '+', 1, 2]
         return input_list
     
@@ -69,13 +78,14 @@ class VirtualMachine:
             # pita nya baca /
             elif pita_value == "/": return a / b
 
-    def run(self, instructions, idx=0):
+    def run(self, instructions, instruction_ke, idx=0):
         step = instructions[idx]
-        print(f'{instructions}\n')
+        print(f'{instructions}')
         while step[0] != "end":
-            print(f'Jalankan instruksi ke {idx}')
+            print(f'\nJalankan line {idx}:')
             print(f'step =  {step}')    
-            if step[0] == "=":
+            if step[0] == "start": print(f'Memory = {self.get_vm_memory()}')
+            elif step[0] == "=":
                 """
                 step =  ['=', 0, '*', 3, '+', 1, 2]
                 """
@@ -120,40 +130,49 @@ class VirtualMachine:
                 print(f'Memory = {self.get_vm_memory()}')
 
             elif step[0] == "goto":
+                
                 """ 
                 kalau udh masuk sini step itu isinya cuman goto<step-berapa>
                 """
                 # ambil index pengen pergi ke step berapa
                 idx = step[1]
                 step = instructions[idx]
+                print(f'Memory = {self.get_vm_memory()}')
                 if step[0] == "end":
+                    print(f'\nJalankan line {idx}:')
                     print(f'step = {step}') 
-                    print("\nFINISHED!")
+                    print(f'Memory = {self.get_vm_memory()}')
+                    print(f"\nINSTRUCTION {instruction_ke} FINISHED!")
                 continue
 
             elif step[0] == "IF":
+                print(f'Memory = {self.get_vm_memory()}')
+                # ['IF', '>', 0, 1, 'goto', 6]
                 condition = [step[1], step[2], step[3]]
                 # math_symbol isinya cuman >, <, >=, <=, ==
                 math_symbol = condition[0]
-                next_step = []
-                for i in range(4, len(step)): next_step.append(step[i])
+
+                # next bakal simpan ["goto","6"]
+                next_step = step[4:]
+                if self.get_vm_memory()[condition[1]] is None: self.get_vm_memory()[condition[1]] = 0
+                if self.get_vm_memory()[condition[2]] is None: self.get_vm_memory()[condition[2]] = 0
                 if math_symbol == ">":
-                    if self.memory[condition[1]] > self.memory[condition[2]]:
+                    if self.get_vm_memory()[condition[1]] > self.get_vm_memory()[condition[2]]:
                         step = next_step
                         continue
 
                 elif math_symbol == "<":
-                    if self.memory[condition[1]] < self.memory[condition[2]]:
+                    if self.get_vm_memory()[condition[1]] < self.get_vm_memory()[condition[2]]:
                         step = next_step
                         continue
 
                 elif math_symbol == ">=":
-                    if self.memory[condition[1]] >= self.memory[condition[2]]:
+                    if self.get_vm_memory()[condition[1]] >= self.get_vm_memory()[condition[2]]:
                         step = next_step
                         continue
 
                 elif math_symbol == "<=":
-                    if self.memory[condition[1]] <= self.memory[condition[2]]:
+                    if self.get_vm_memory()[condition[1]] <= self.memory[condition[2]]:
                         step = next_step
                         continue
 
@@ -163,10 +182,19 @@ class VirtualMachine:
                         continue
 
             idx += 1
-            step = instructions[idx]
-            print(f'next step {step}')
-            if step[0] == "end": print("\nFINISHED!")
-            print()
+            try:
+                step = instructions[idx]
+            except: 
+                print("\nERROR!")
+                print("Possible Error: Typo in input!\n")
+                break
+
+            print(f'next step adalah line {idx}: {step}')
+            if step[0] == "end":
+                print(f'\nJalankan line {idx}:')
+                print(f'step = {step}') 
+                print(f'Memory = {self.get_vm_memory()}')
+                print(f"\nINSTRUCTION {instruction_ke} FINISHED!")
 
 def readTxtFile(txtFileName):
     with open(txtFileName) as f: theFile = f.readlines()
@@ -178,17 +206,32 @@ def make_clean_instructons():
     for i in range(len(clean_instructons)): clean_instructons[i] = vm.change_number_to_integer(clean_instructons[i])
     return clean_instructons
 
+def print_instructions(instructions,instruction_ke,step=0):
+    instruction_table = PrettyTable(["Line","Instruction"])
+    instruction_table.title = f"INSTRUCTION {instruction_ke}"
+    instruction_table.hrules=ALL
+    for line in instructions: 
+        step = str(step)+":"
+        instruction_table.add_row([step,line])
+        step = int(step[:-1])
+        step+=1
+    return instruction_table
+
 if __name__ == "__main__":
-    print("\nSTART!")
+    user_input = input("\nRun Instruction Ke: ")
     vm = VirtualMachine()
 
-    instructions = readTxtFile('./instructions/instruction-1.txt')
+    instructions = readTxtFile(f'./instructions/instruction-{user_input}.txt')
     # instructions = ['(start)\n', '(= 0 (* 3( + 1 2)))\n', '(= 1 (* 3( + 0 2)))\n', '(IF (> 0 1) (goto 6))\n', '(= 2 (0))\n', '(goto 7)\n', '(= 2 (1))\n', '(end)\n']
     # note: karena masih ada /n nya pas baca txt kita harus bersihin /n nya dari instructions nya makanya dibuat function make_clean_instructons
+
+    instructions_table = print_instructions(instructions,user_input)
+    print(instructions_table)
 
     clean_instructions = make_clean_instructons()
     # clean_instructions = ['(start)', '(= 0 (* 3( + 1 2)))', '(= 1 (* 3( + 0 2)))', '(IF (> 0 1) (goto 6))', '(= 2 (0))', '(goto 7)', '(= 2 (1))', '(end)']
 
     initial_memory = vm.get_vm_memory()
-    print(f'Memory = {initial_memory}')
-    vm.run(clean_instructions)
+    print(f"\nSTART INSTRUCTION {user_input}!\n")
+    print(f"Initial Memory = {initial_memory}")
+    vm.run(clean_instructions,user_input)
